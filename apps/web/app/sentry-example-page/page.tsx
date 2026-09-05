767a28c0898401487a35a4786a91345adeee9226
Yash Giradkar <yashgiradkar02@gmail.com>
2026-09-06 00:23:12 +0530

    fix(web): fix eslint configs, tsconfig ignoreDeprecations, and sentry example links

--- apps/web/app/sentry-example-page/page.tsx
+++ apps/web/app/sentry-example-page/page.tsx
@@ -52,7 +52,7 @@ export default function Page() {
           Click the button below, and view the sample error on the Sentry{" "}
           <a
             target="_blank"
-            rel="noopener"
+            rel="noopener noreferrer"
             href="https://aivon-2t.sentry.io/issues/?project=4511919007334400"
           >
             Issues Page
@@ -60,7 +60,7 @@ export default function Page() {
           . For more details about setting up Sentry,{" "}
           <a
             target="_blank"
-            rel="noopener"
+            rel="noopener noreferrer"
             href="https://docs.sentry.io/platforms/javascript/guides/nextjs/"
           >
             read our docs
